f1c877285d56cbd576a40c0cdb30b4e625eab4b0
Mohammad <malnajjar16@gmail.com>
2021-12-14 00:27:31 +0100

    forms middleware is added

--- server/index.js
+++ server/index.js
@@ -5,30 +5,31 @@ import { connectDB } from './config/connectDB.js';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import colors from 'colors';
-
 import { router as usersRouter } from './routes/users.js';
+
 // env settings
 const __dirname = path.resolve();
 dotenv.config({ path: `${__dirname}/config/.env` });
 
 const app = express();
 const PORT = process.env.PORT || 4444;
-
 // middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
 
 // routes
 app.get('/', (req, res) => {
-  res.send('Worked ...');
+    res.send('Worked ...');
 });
-app.use('/', usersRouter);
+app.use('/api/users', usersRouter);
+
 // server init function
 const startServer = () => {
-  // db connect
-  connectDB();
-  app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT} ...`.yellow));
+    // db connect
+    connectDB();
+    app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT} ...`.yellow));
 };
 
 startServer();
