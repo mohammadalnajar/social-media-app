83b9852d904e495e0b76238058bf04d528d08d0d
Mohammad <malnajjar16@gmail.com>
2021-12-14 00:29:08 +0100

    session config added for later usage

---
+++ server/config/session.js
@@ -0,0 +1,14 @@
+import MongoStore from 'connect-mongo';
+import session from 'express-session';
+const sessionConfig = () => {
+    const sessionConfigObj = session({
+        store: new MongoStore({
+            mongoUrl: process.env.MONGODB_URL
+        }),
+        secret: 'blablabla',
+        cookie: { maxAge: 100000 },
+        saveUninitialized: false
+    });
+    return sessionConfigObj;
+};
+export default sessionConfig;
