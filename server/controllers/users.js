ac52051689d3c0c0eb203444202df534bd9ff72a
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-10 23:25:19 +0100

    finish register route with handeling errors

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -12,20 +12,20 @@ export const registerUser = async (req, res) => {
     const { userName, email, phone, password } = req.body;
     const salt = 10;
     try {
-        const response = await User.validate({ userName, email, phone, password });
         const hashedPass = await bcrypt.hash(password, salt);
+        const response = await User.create({ userName, email, phone, password: hashedPass });
         console.log(response);
         res.json(response);
-        // console.log(res, 'test');
-        // const res = await User.create({ userName, email, phone, password });
-        // console.log(res, 'response logged...');
-        // if (res) {
-        //   console.log('Is registered...');
-        // }
     } catch (error) {
         const errorMessage = getErrorMessage(error.message);
-        // console.log(error, 'error from register route ... ');
-        return res.status(400).json({ status: 'failed', errorMessage });
+        ['email', 'userName'].forEach((field) => {
+            if (error.code === 11000 && error.keyPattern[field]) {
+                errorMessage.push(`${field} should be unique ... `);
+            }
+        });
+        return res
+            .status(400)
+            .json({ status: 'failed to register', errorMessage, errorMsg: error });
     }
 };
 
