5581b2f51e513f21d671bd14aa39228c80cd35cf
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-10 23:24:53 +0100

    delete only numbers validation

--- server/models/UserValidations.js
+++ server/models/UserValidations.js
@@ -3,10 +3,10 @@ import { patterns } from '../utils/validationPatterns.js';
 const { email, name, phone, password } = patterns;
 const { lowercaseLetter, uppercaseLetter, numbers, specialCharacters, min } = password;
 export const usernameValidations = [
-    {
-        validator: (val) => name.test(val),
-        message: '__error__Please insert only letters ...__error_end__'
-    },
+    // {
+    //     validator: (val) => name.test(val),
+    //     message: '__error__Please insert only letters ...__error_end__'
+    // },
     {
         validator: (val) => val.length < 20,
         message: '__error__A username cannot contains more than 20 characters! ...__error_end__'
