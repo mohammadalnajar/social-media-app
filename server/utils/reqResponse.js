cbd1d1017fb39adfbc05f602de76d27ba3986e72
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-12 00:11:23 +0100

    modify reqresponse func

--- server/utils/reqResponse.js
+++ server/utils/reqResponse.js
@@ -1,7 +1,9 @@
 export const errorRes = (res, statusCode, status, errorMessages, error) => {
-    return res.status(statusCode).json({ status, errorMessages, error });
+    return res
+        .status(statusCode)
+        .json({ status: status, errorMessages: errorMessages, error: error });
 };
 
 export const successRes = (res, statusCode, status, msg, data) => {
-    return res.status(statusCode).json({ status, msg, data });
+    return res.status(statusCode).json({ status: status, msg: msg, data: data });
 };
