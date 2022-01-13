834eed289d060c8f82c296a85d37e98efb1b70d5
Hani al Sabti <h.sabti87@gmail.com>
2022-01-13 19:33:32 +0100

    configured react query

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import './index.css';
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
-        <div className="bg-gray-800">
-            <span className="text-gray-100">hello world</span>
-        </div>
+        <QueryClientProvider client={queryClient}>
+            <div className="h-screen bg-gray-800 flex flex-col justify-center items-center">
+                <p className="text-gray-100">hello world</p>
+            </div>
+        </QueryClientProvider>
     );
 }
 
