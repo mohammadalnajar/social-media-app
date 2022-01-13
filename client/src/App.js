829e9974aa6e1c2cb41530e9451b252ec3af5d43
Hani al Sabti <h.sabti87@gmail.com>
2022-01-13 18:02:29 +0100

    moved index.css to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './index.css';
 
 function App() {
-    return <div className="App" />;
+    return (
+        <div className="bg-gray-800">
+            <span className="text-gray-100">hello world</span>
+        </div>
+    );
 }
 
 export default App;
