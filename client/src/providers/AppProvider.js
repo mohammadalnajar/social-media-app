07bfef7e9143d31c7e5194d604e7b9a855ab8358
Hani al Sabti <h.sabti87@gmail.com>
2022-01-14 22:16:40 +0100

    adjusted tab width

--- client/src/providers/AppProvider.js
+++ client/src/providers/AppProvider.js
@@ -5,40 +5,38 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 const ErrorFallback = () => {
-    <div
-        className="text-red-500 w-screen h-screen flex flex-col justify-center items-center"
-        role="alert"
+  <div
+    className="text-red-500 w-screen h-screen flex flex-col justify-center items-center"
+    role="alert"
+  >
+    <h2 className="text-lg font-semibold">Ooops, something went wrong :(</h2>
+    <button
+      type="button"
+      className="mt-4"
+      onClick={() => {
+        window.location.assign(window.location.origin);
+      }}
     >
-        <h2 className="text-lg font-semibold">
-            Ooops, something went wrong :(
-        </h2>
-        <button
-            type="button"
-            className="mt-4"
-            onClick={() => {
-                window.location.assign(window.location.origin);
-            }}
-        >
-            Refresh
-        </button>
-    </div>;
+      Refresh
+    </button>
+  </div>;
 };
 
 const queryClient = new QueryClient();
 
 const AppProvider = ({ children }) => {
-    return (
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
-            <QueryClientProvider client={queryClient}>
-                {children}
-                <ReactQueryDevtools initialIsOpen={false} />
-            </QueryClientProvider>
-        </ErrorBoundary>
-    );
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ErrorBoundary>
+  );
 };
 
 AppProvider.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default AppProvider;
