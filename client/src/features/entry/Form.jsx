2760b6c84e2886ccc7169182cc80fb7e541ebb1b
Hani al Sabti <h.sabti87@gmail.com>
2022-01-14 19:47:10 +0100

    wrapped inputs inside form element

--- client/src/features/entry/Form.jsx
+++ client/src/features/entry/Form.jsx
@@ -7,8 +7,8 @@ const Form = () => {
         className="hidden bg-cover lg:block lg:w-1/2"
         style={{
           backgroundImage:
-            "url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')",
-          objectFit: 'cover',
+            "url('https://images.unsplash.com/photo-1611162617213-7d7a39e9b1d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80')",
+          backgroundPosition: 'center',
         }}
       />
 
@@ -54,74 +54,60 @@ const Form = () => {
         <div className="flex items-center justify-between mt-4">
           <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4" />
 
-          <button
-            type="button"
-            href="#"
-            className="text-xs text-center text-gray-500 uppercase dark:text-gray-400 hover:underline"
-          >
+          <span className="text-xs text-center text-gray-500 uppercase dark:text-gray-400">
             or login with email
-          </button>
+          </span>
 
           <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4" />
         </div>
-
-        <div className="mt-4">
-          <label
-            className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-            htmlFor="LoggingEmailAddress"
-          >
-            Email Address
+        <form>
+          <div className="mt-4">
+            <span className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
+              Email Address
+            </span>
             <input
               id="LoggingEmailAddress"
               className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
               type="email"
             />
-          </label>
-        </div>
-
-        <div className="mt-4">
-          <div className="flex justify-between">
-            <span
-              className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-              htmlFor="loggingPassword"
-            >
-              Password
-            </span>
-            <button
-              type="button"
-              href="#"
-              className="text-xs text-gray-500 dark:text-gray-300 hover:underline"
-            >
-              Forget Password?
-            </button>
           </div>
 
-          <input
-            id="loggingPassword"
-            className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-            type="password"
-          />
-        </div>
+          <div className="mt-4">
+            <div className="flex justify-between">
+              <span className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
+                Password
+              </span>
+              <button
+                type="button"
+                className="text-xs text-gray-500 dark:text-gray-300 hover:underline mb-2"
+              >
+                Forget Password?
+              </button>
+            </div>
+            <input
+              id="loggingPassword"
+              className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+              type="password"
+            />
+          </div>
 
-        <div className="mt-8">
-          <button
-            type="submit"
-            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
-          >
-            Login
-          </button>
-        </div>
-
+          <div className="mt-8">
+            <button
+              type="submit"
+              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+            >
+              Login
+            </button>
+          </div>
+        </form>
         <div className="flex items-center justify-between mt-4">
           <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4" />
-
           <button
             type="button"
             className="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
           >
             or sign up
           </button>
-
           <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4" />
         </div>
       </div>
