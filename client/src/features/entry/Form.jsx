ac1dc37805d41ef58cd100e1331ab7fdffa46ee4
Hani al Sabti <h.sabti87@gmail.com>
2022-01-14 22:04:27 +0100

    refactored form

--- client/src/features/entry/Form.jsx
+++ client/src/features/entry/Form.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Form = () => {
   return (
-    <div className="flex w-full mx-auto overflow-hidden bg-white rounded-lg drop-shadow-2xl dark:bg-gray-800 lg:max-w-4xl">
+    <div className="flex w-full mx-auto overflow-hidden bg-white p-6 lg:p-0 lg:drop-shadow-2xl dark:bg-gray-800 lg:max-w-4xl">
       <div
-        className="hidden bg-cover lg:block lg:w-1/2"
+        className="hidden bg-cover md:block md:w-2/3"
         style={{
           backgroundImage:
             "url('https://images.unsplash.com/photo-1611162617213-7d7a39e9b1d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80')",
@@ -13,13 +13,9 @@ const Form = () => {
       />
 
       <div className="w-full px-6 py-8 md:px-8 lg:w-1/2 my-auto">
-        <h2 className="text-2xl font-semibold text-center text-gray-700 dark:text-white">
-          Brand
+        <h2 className="text-4xl font-bold text-center text-gray-700 dark:text-white mb-6">
+          The Social App
         </h2>
-
-        <p className="text-xl text-center text-gray-600 dark:text-gray-200">
-          Welcome back!
-        </p>
 
         <button
           type="button"
