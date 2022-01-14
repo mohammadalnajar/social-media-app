947125d265e9560f785f0b03322b8aa3e438ce2b
Hani al Sabti <h.sabti87@gmail.com>
2022-01-14 16:00:23 +0100

    Added static login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Form from './features/entry/Form';
 import './index.css';
 import AppProvider from './providers/AppProvider';
 
 const App = () => {
     return (
         <AppProvider>
-            <div className="h-screen bg-gray-800 flex flex-col justify-center items-center">
-                <p className="text-gray-100">hello world</p>
+            <div className="h-screen w-screen bg-white dark:bg-gray-800 flex flex-col justify-center items-center">
+                <Form />
             </div>
         </AppProvider>
     );
