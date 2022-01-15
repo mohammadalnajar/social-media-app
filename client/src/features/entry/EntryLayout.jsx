c26b8f1a83cdc25cc93ce250ffbe09563a40a6df
Hani al Sabti <h.sabti87@gmail.com>
2022-01-15 15:48:11 +0100

    added state to toggle forms

--- client/src/features/entry/EntryLayout.jsx
+++ client/src/features/entry/EntryLayout.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormButton from './components/FormButton';
 import FormHeading from './components/FormHeading';
 import FormToggle from './components/FormToggle';
 import FormWrapper from './components/FormWrapper';
 import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
 
 const EntryLayout = () => {
+  const [login, setLogin] = useState(true);
+  const toggleForm = () => {
+    setLogin(!login);
+  };
   return (
     <FormWrapper>
-      <FormHeading />
-      <LoginForm>
-        <FormButton title="Login" />
-      </LoginForm>
-      <FormToggle title="or sign up" />
+      {login ? (
+        <>
+          <FormHeading />
+          <LoginForm>
+            <FormButton title="Login" />
+          </LoginForm>
+        </>
+      ) : (
+        <RegisterForm>
+          <FormButton title="Sign up" />
+        </RegisterForm>
+      )}
+
+      <FormToggle
+        title={login ? 'or sign up' : 'or login'}
+        toggleForm={toggleForm}
+      />
     </FormWrapper>
   );
 };
