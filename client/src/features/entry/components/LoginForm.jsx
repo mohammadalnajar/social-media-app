e91171d7a0a49bf23c0a392bd13a4fd6f3f0df20
Hani al Sabti <h.sabti87@gmail.com>
2022-01-15 18:22:32 +0100

    changed input id to name

--- client/src/features/entry/components/LoginForm.jsx
+++ client/src/features/entry/components/LoginForm.jsx
@@ -9,7 +9,7 @@ const LoginForm = ({ children }) => {
           Email Address
         </span>
         <input
-          id="LoggingEmailAddress"
+          name="email"
           className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
           type="email"
           autoComplete="email"
@@ -29,7 +29,7 @@ const LoginForm = ({ children }) => {
           </button>
         </div>
         <input
-          id="loggingPassword"
+          name="password"
           className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
           type="password"
           autoComplete="password"
--- client/src/features/entry/components/RegisterForm.jsx
+++ client/src/features/entry/components/RegisterForm.jsx
@@ -10,6 +10,7 @@ const RegisterForm = ({ children }) => {
             First name
           </span>
           <input
+            name="firstName"
             type="text"
             autoComplete="given-name"
             className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
@@ -20,6 +21,7 @@ const RegisterForm = ({ children }) => {
             Last name
           </span>
           <input
+            name="lastName"
             type="text"
             autoComplete="family-name"
             className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
@@ -31,7 +33,7 @@ const RegisterForm = ({ children }) => {
           Email Address
         </span>
         <input
-          id="LoggingEmailAddress"
+          name="email"
           className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
           type="email"
           autoComplete="email"
@@ -42,7 +44,7 @@ const RegisterForm = ({ children }) => {
           Password
         </span>
         <input
-          id="loggingPassword"
+          name="password"
           className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
           type="password"
           autoComplete="password"
