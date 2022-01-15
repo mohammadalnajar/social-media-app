01aaa1dc71b1bf267d521b5f317c4fecfd95323c
Hani al Sabti <h.sabti87@gmail.com>
2022-01-16 00:11:56 +0100

    added header to fetcher

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,27 +1,30 @@
-const URL = 'http://localhost:4444/api/users/';
+const SERVER_URL = 'http://localhost:4444/api/users/';
 
 const handleApiResponse = async (response) => {
   try {
     const data = await response.json();
+    console.log(data, 'response');
     if (response.ok) {
       return data;
     }
   } catch (error) {
     console.log(error);
   }
-  return false;
+  return null;
 };
 
 export const loginWithEmailAndPassword = async (data) => {
-  return fetch(`${URL}signin`, {
+  return fetch(`${SERVER_URL}signin`, {
     method: 'POST',
+    headers: { 'content-type': 'application/json' },
     body: JSON.stringify(data),
   }).then(handleApiResponse);
 };
 
 export const registerWithEmailAndPassword = async (data) => {
-  return fetch(`${URL}signup`, {
+  return fetch(`${SERVER_URL}signup`, {
     method: 'POST',
+    headers: { 'content-type': 'application/json' },
     body: JSON.stringify(data),
   }).then(handleApiResponse);
 };
