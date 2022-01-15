d041dc21c48c8cc0df180b9b9f875d575b4c6b98
Hani al Sabti <h.sabti87@gmail.com>
2022-01-15 15:07:11 +0100

    added required prop to form button

--- client/src/features/entry/components/FormButton.jsx
+++ client/src/features/entry/components/FormButton.jsx
@@ -1,18 +1,21 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const FormButton = () => {
+const FormButton = ({ title }) => {
   return (
-    <div className="flex items-center justify-between mt-4">
-      <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4" />
+    <div className="mt-8">
       <button
-        type="button"
-        className="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
+        type="submit"
+        className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
       >
-        or sign up
+        {title}
       </button>
-      <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4" />
     </div>
   );
+};
+
+FormButton.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default FormButton;
