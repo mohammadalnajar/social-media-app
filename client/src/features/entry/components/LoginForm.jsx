352a8f8286fad276d04901fdcd10ab1639101761
Hani al Sabti <h.sabti87@gmail.com>
2022-01-15 15:57:29 +0100

    added autocomplete to inputs

--- client/src/features/entry/components/LoginForm.jsx
+++ client/src/features/entry/components/LoginForm.jsx
@@ -12,6 +12,7 @@ const LoginForm = ({ children }) => {
           id="LoggingEmailAddress"
           className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
           type="email"
+          autoComplete="email"
         />
       </div>
 
@@ -31,6 +32,7 @@ const LoginForm = ({ children }) => {
           id="loggingPassword"
           className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
           type="password"
+          autoComplete="password"
         />
       </div>
       {children}
--- client/src/features/entry/components/RegisterForm.jsx
+++ client/src/features/entry/components/RegisterForm.jsx
@@ -11,6 +11,7 @@ const RegisterForm = ({ children }) => {
           </span>
           <input
             type="text"
+            autoComplete="given-name"
             className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
           />
         </div>
@@ -20,6 +21,7 @@ const RegisterForm = ({ children }) => {
           </span>
           <input
             type="text"
+            autoComplete="family-name"
             className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
           />
         </div>
@@ -32,6 +34,7 @@ const RegisterForm = ({ children }) => {
           id="LoggingEmailAddress"
           className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
           type="email"
+          autoComplete="email"
         />
       </div>
       <div className="mt-4">
@@ -42,6 +45,7 @@ const RegisterForm = ({ children }) => {
           id="loggingPassword"
           className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
           type="password"
+          autoComplete="password"
         />
       </div>
       {children}
