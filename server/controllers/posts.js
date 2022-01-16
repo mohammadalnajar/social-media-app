7e15d2d27cf923d5f687e1bbdc095ccd0542b5ca
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-16 22:27:20 +0100

    posts controllers basic building

---
+++ server/controllers/posts.js
@@ -0,0 +1,16 @@
+// ========= get ALL posts =========
+export const getPosts = (req, res) => {
+    res.send('get posts');
+};
+// ========= create a post =========
+export const createPost = (req, res) => {
+    res.send('create post');
+};
+// ========= update a post =========
+export const updatePost = (req, res) => {
+    res.send('update post');
+};
+// ========= delete a post =========
+export const deletePost = (req, res) => {
+    res.send('delete post');
+};
