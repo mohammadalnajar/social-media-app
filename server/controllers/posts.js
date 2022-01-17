e725edbfadb278be73116f6c03c268ed58c0eed9
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-17 01:21:46 +0100

    create post controller done

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,10 +1,54 @@
-// ========= get ALL posts =========
-export const getPosts = (req, res) => {
-    res.send('get posts');
+import Post from '../models/Post.js';
+import User from '../models/User.js';
+import { errorRes, successRes } from '../utils/reqResponse.js';
+
+// ========= get ALL posts (everyone) =========
+export const getAllUsersPosts = (req, res) => {
+    res.send('getAllUsersPosts');
+};
+// ========= get ALL posts (friends) =========
+export const getAllFriendsPosts = (req, res) => {
+    res.send('getAllFriendsPosts');
+};
+// ========= get ALL posts (user) =========
+export const getAllUserPosts = (req, res) => {
+    res.send('getAllUserPosts');
 };
 // ========= create a post =========
-export const createPost = (req, res) => {
-    res.send('create post');
+export const createPost = async (req, res) => {
+    // get userId from session
+    const { _id: userId } = req.session.userData;
+
+    // create post
+    const { text, visibility } = req.body;
+    try {
+        const createdPost = await Post.create({ text, visibility });
+        if (createdPost) {
+            const updated = await User.findOneAndUpdate(
+                { _id: userId },
+                {
+                    $push: { posts: createdPost.id },
+                }
+            );
+            if (updated) {
+                const user = await User.findById(userId);
+                const { password, isAdmin, ...rest } = user._doc;
+                req.session.userData = rest;
+                return successRes(res, 200, 'ok', 'post is created', rest);
+            }
+            return errorRes(
+                res,
+                400,
+                'failed to add the post created to the user ...',
+                null,
+                null
+            );
+        }
+        return errorRes(res, 400, 'failed to create a post ...', null, null);
+    } catch (error) {
+        console.log(error, 'error in create post ...');
+        return errorRes(res, 500, 'server error');
+    }
 };
 // ========= update a post =========
 export const updatePost = (req, res) => {
