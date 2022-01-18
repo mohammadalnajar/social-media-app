2dca90f918fe83971bc6ece5546723593f75f2b2
Hani al Sabti <h.sabti87@gmail.com>
2022-01-18 23:46:46 +0100

    created basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import EntryLayout from './features/entry/EntryLayout';
+import MainLayout from './components/layout/MainLayout';
 import './index.css';
 import AppProvider from './providers/AppProvider';
 
 const App = () => {
   return (
     <AppProvider>
-      <div className="h-screen w-screen bg-white dark:bg-gray-800 flex flex-col justify-center items-center">
-        <EntryLayout />
+      <div className="h-screen w-screen bg-white dark:bg-dark-second">
+        {/* <EntryLayout /> */}
+        <MainLayout />
       </div>
     </AppProvider>
   );
