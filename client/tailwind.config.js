cb6cec54e86318f666ce6a208feef7d6be97026f
Hani al Sabti <h.sabti87@gmail.com>
2022-01-18 23:46:02 +0100

    added custom colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-main': '#18191A',
+        'dark-second': '#242526',
+        'dark-third': '#3A3B3C',
+        'dark-txt': '#B8BBBF',
+      },
+    },
   },
   plugins: [],
 };
