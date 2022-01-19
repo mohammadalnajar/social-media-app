d33ba1d201b6dc3a20bd7ccb95cc86d9977e1f09
Hani al Sabti <h.sabti87@gmail.com>
2022-01-19 19:19:00 +0100

    fixed search input size on tablet size

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainLayout = () => {
   return (
-    <div className="bg-gray-100 dark:bg-dark-main">
+    <div className="bg-gray-100 dark:bg-dark-main h-screen w-screen">
       {/* <!-- NAV --> */}
       <nav className="bg-white dark:bg-dark-second h-max my-auto md:h-14 w-full shadow flex flex-col md:flex-row items-center justify-center md:justify-between fixed top-0 z-50 border-b dark:border-dark-third">
         {/* <!-- LEFT NAV --> */}
@@ -26,7 +26,7 @@ const MainLayout = () => {
             />
           </button>
           <div className="flex items-center justify-between space-x-1 space-x">
-            <div className="relative bg-gray-100 dark:bg-dark-third px-2 py-2 w-10 h-10 sm:w-11 sm:h-11 lg:h-10 lg:w-10 xl:w-max xl:pl-3 xl:pr-8 rounded-full flex items-center justify-center cursor-pointer">
+            <div className="relative bg-gray-100 dark:bg-dark-third px-2 py-2 w-10 h-10 md:w-11 md:h-11 xl:w-max xl:pl-3 xl:pr-8 rounded-full flex items-center justify-center cursor-pointer">
               <i className="bx bx-search-alt-2 text-xl xl:mr-2 dark:text-dark-txt" />
               <input
                 type="text"
