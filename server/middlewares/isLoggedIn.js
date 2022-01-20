412b282a266ad8f607ccd6e929c91e44893b3d03
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-20 01:51:19 +0100

    changed message in the backend

--- server/middlewares/isLoggedIn.js
+++ server/middlewares/isLoggedIn.js
@@ -4,7 +4,7 @@ const isUserLoggedIn = (req, res, next) => {
     if (req.session.userData) {
         return next();
     }
-    return errorRes(res, 404, 'not found', null, null);
+    return errorRes(res, 404, 'not authenticated', null, null);
 };
 
 export default isUserLoggedIn;
