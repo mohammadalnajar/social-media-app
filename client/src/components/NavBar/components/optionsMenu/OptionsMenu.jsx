37c26b2e461c39a64f36ea029bb1af2d530b70a5
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-21 18:36:22 +0100

    gave hover effect

--- client/src/components/NavBar/components/optionsMenu/OptionsMenu.jsx
+++ client/src/components/NavBar/components/optionsMenu/OptionsMenu.jsx
@@ -8,7 +8,7 @@ const OptionsMenu = () => {
         <button
           type="button"
           href="#"
-          className="inline-flex items-center justify-center p-1 rounded-full hover:bg-gray-200 dark:hover:bg-dark-third mx-1"
+          className="inline-flex items-center justify-center p-1 rounded-full hover:bg-gray-200 dark:hover:bg-dark-third mx-1 hover:opacity-80 hover:cursor-pointer"
         >
           <img
             src="./assets/images/tuat.jpg"
