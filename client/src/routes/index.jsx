80e27c2a575a2a07422cc3caeaab8ffd045732b9
Hani al Sabti <h.sabti87@gmail.com>
2022-01-21 12:27:27 +0100

    removed unused code

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,33 +1,17 @@
-import React, { useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import { useQuery } from 'react-query';
-import EntryLayout from '../features/entry/EntryLayout';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import MainLayout from '../components/layout/MainLayout';
+import EntryLayout from '../features/entry/EntryLayout';
 import ProtectedRoute from './protected';
-import { fetchUser } from '../utils/api';
-import LoadingPage from '../components/LoadingPage';
 
 const AppRoutes = () => {
-  const { isSuccess, error } = useQuery('fetchUser', fetchUser);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (isSuccess) {
-      // if user logged in
-      navigate('/feed');
-    }
-    if (error) {
-      // user not logged in
-      navigate('/');
-    }
-  }, [isSuccess, error]);
   return (
     <Routes>
       <Route path="/" element={<EntryLayout />} />
-      <Route path="/loading" element={<LoadingPage />} />
       <Route
         path="/feed"
         element={
-          <ProtectedRoute isSuccess={isSuccess} redirectTo="/loading">
+          <ProtectedRoute redirectTo="/">
             <MainLayout />
           </ProtectedRoute>
         }
