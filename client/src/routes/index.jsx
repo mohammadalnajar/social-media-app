ccfabe439c0a84204e665a63763a8b14e162d940
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-21 01:58:47 +0100

    fetching the user data on refresh

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-
+import React, { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useQuery } from 'react-query';
 import EntryLayout from '../features/entry/EntryLayout';
 import MainLayout from '../components/layout/MainLayout';
 import ProtectedRoute from './protected';
+import { fetchUser } from '../utils/api';
+import LoadingPage from '../components/LoadingPage';
 
 const AppRoutes = () => {
+  const { isSuccess, error } = useQuery('fetchUser', fetchUser);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isSuccess) {
+      // if user logged in
+      navigate('/feed');
+    }
+    if (error) {
+      // user not logged in
+      navigate('/');
+    }
+  }, [isSuccess, error]);
   return (
     <Routes>
       <Route path="/" element={<EntryLayout />} />
+      <Route path="/loading" element={<LoadingPage />} />
       <Route
         path="/feed"
         element={
-          <ProtectedRoute redirectTo="/">
+          <ProtectedRoute isSuccess={isSuccess} redirectTo="/loading">
             <MainLayout />
           </ProtectedRoute>
         }
