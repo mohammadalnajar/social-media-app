501efae784a9341555ced5c7c84723bac7764f44
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-22 10:18:10 +0100

    toggle dark mode

--- client/src/components/NavBar/components/DarkModeMoon.jsx
+++ client/src/components/NavBar/components/DarkModeMoon.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import useDarkMode from '../../../hooks/useDarkMode';
+import useLocalStorage from '../../../hooks/useLocalStorage';
 
 const DarkModeMoon = ({ extraClasses }) => {
+  const [storedValue, setValue] = useLocalStorage('darkMode');
+  const [colorTheme, setTheme] = useDarkMode(storedValue);
+
+  const darkModeToggle = () => {
+    setValue(storedValue === 'dark' ? 'light' : 'dark');
+    setTheme(colorTheme);
+  };
+
   return (
-    <div
+    <button
+      type="button"
+      onClick={darkModeToggle}
       className={`${extraClasses} text-2xl grid place-items-center md:hidden bg-gray-200 dark:bg-dark-third rounded-full w-10 h-10 cursor-pointer hover:bg-gray-300 dark:text-dark-txt`}
       id="dark-mode-toggle-mb"
     >
       <i className="bx bxs-moon" />
-    </div>
+    </button>
   );
 };
 DarkModeMoon.propTypes = {
