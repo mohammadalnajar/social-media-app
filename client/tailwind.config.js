72be496bb4cc7beb2879c56000d80bbbbf9d8855
Hani al Sabti <h.sabti87@gmail.com>
2022-01-22 12:15:29 +0100

    added additional screen breakpoints

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      xs: '475px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         'dark-main': '#18191A',
