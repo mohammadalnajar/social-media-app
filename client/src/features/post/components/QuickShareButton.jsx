314abf3ad9ac9e9e399dffa9b922e379024fafec
Hani al Sabti <h.sabti87@gmail.com>
2022-01-22 12:22:39 +0100

    made text hidden on xs screen size

--- client/src/features/post/components/QuickShareButton.jsx
+++ client/src/features/post/components/QuickShareButton.jsx
@@ -5,7 +5,7 @@ const QuickShareButton = ({ title, iconType, color }) => {
   return (
     <div className="w-1/3 flex space-x-2 justify-center items-center hover:bg-gray-100 dark:hover:bg-dark-third text-xl sm:text-3xl py-2 rounded-lg cursor-pointer text-red-500">
       <i className={`bx ${iconType} ${color}`} />
-      <span className="text-xs sm:text-sm font-semibold text-gray-500 dark:text-dark-txt">
+      <span className="hidden xs:inline text-xs sm:text-sm font-semibold text-gray-500 dark:text-dark-txt">
         {title}
       </span>
     </div>
