25ce8bddeb272374e65518ccd747ca1f246016b9
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-23 08:06:40 +0100

    dropdown icon made

---
+++ client/src/components/NavBar/components/optionsMenu/dropdown/DropdownIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DropdownIcon = () => {
+  return (
+    <div className="text-xl hidden xl:grid place-items-center bg-gray-200 dark:bg-dark-third dark:text-dark-txt rounded-full mx-1 p-3 cursor-pointer hover:bg-gray-300 relative">
+      <i className="fas fa-chevron-down" />
+    </div>
+  );
+};
+
+export default DropdownIcon;
