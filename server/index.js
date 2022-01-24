4d763a4dde6e261ce2ad9b9270790649efeaf9bd
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-24 09:18:06 +0100

    made public server images folder

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ dotenv.config({ path: `${dirname}/config/.env` });
 const app = express();
 const PORT = process.env.PORT || 4444;
 // middleware
+app.use('/images', express.static(path.join(dirname, 'public/images')));
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
