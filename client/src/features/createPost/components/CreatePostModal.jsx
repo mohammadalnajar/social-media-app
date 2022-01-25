5d63d333d11d4f64d6f1f0cc6e059c9c65b1469f
Hani al Sabti <h.sabti87@gmail.com>
2022-01-25 13:51:00 +0100

    linked query functions to upload and create post

--- client/src/features/createPost/components/CreatePostModal.jsx
+++ client/src/features/createPost/components/CreatePostModal.jsx
@@ -1,28 +1,67 @@
 import PropTypes from 'prop-types';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import { useMutation } from 'react-query';
 import useForm from '../../../hooks/useForm';
 import useOnClickOutside from '../../../hooks/useOnClickOutside';
+import { createPost, uploadImage } from '../../../utils/api';
 import capitalize from '../../../utils/helpers';
 import CreatePostSelect from './CreatePostSelect';
+import ImageDropzone from './ImageDropzone';
+import ImagePreview from './ImagePreview';
 
 const CreatePostModal = ({ isOpen, setIsOpen, firstName, children }) => {
-  const { formData, handleInputChange } = useForm({
+  const [files, setFiles] = useState([]);
+  const [select, setSelect] = useState('select visibility');
+  const [showDropzone, setShowDropzone] = useState(false);
+  const { formData, handleInputChange, reset } = useForm({
     text: '',
   });
+
+  const addPost = useMutation(createPost, {
+    onSuccess: () => {
+      setFiles([]);
+      reset();
+      setSelect('select visibility');
+      setShowDropzone(false);
+      setIsOpen(false);
+    },
+  });
+
+  const upload = useMutation(uploadImage, {
+    onSuccess: (data) => {
+      const {
+        data: { imageUrl },
+      } = data;
+      const newFormData = { ...formData, visibility: select, imageUrl };
+      addPost.mutate(newFormData);
+    },
+  });
+
+  const handleSubmit = () => {
+    upload.mutate(files);
+  };
+
   const ref = useRef();
   useOnClickOutside(ref, () => {
     setIsOpen(false);
   });
+
+  const toggleDropzone = () => {
+    setShowDropzone(!showDropzone);
+    if (showDropzone) {
+      setFiles([]);
+    }
+  };
 
   return (
     <>
       <input type="checkbox" id="my-modal-2" className="modal-toggle" />
       <div className={`modal ${isOpen ? 'modal-open' : ''}`}>
         <div ref={ref} className="modal-box dark:bg-dark-second">
-          <div className="card">
+          <div className="card overflow-visible">
             {children}
-            <div className="divider dark:before:bg-dark-third dark:after:bg-dark-third" />
-            <div className="flex">
+            <div className="divider dark:before:bg-dark-third dark:after:bg-dark-third my-2" />
+            <div className="flex px-3 max-h-16">
               <div className="avatar">
                 <div className="mb-8 rounded-full w-14 h-14">
                   <img
@@ -35,10 +74,10 @@ const CreatePostModal = ({ isOpen, setIsOpen, firstName, children }) => {
                 <h1 className="text-gray-900 dark:text-dark-txt font-semibold capitalize">
                   {firstName}
                 </h1>
-                <CreatePostSelect />
+                <CreatePostSelect select={select} setSelect={setSelect} />
               </div>
             </div>
-            <div className="form-control">
+            <div className="form-control px-3 max-h-80 scrollbar-thin hover:scrollbar-thumb-gray-300 dark:hover:scrollbar-thumb-dark-hover scrollbar-track-transparent overflow-y-auto">
               <textarea
                 name="text"
                 value={formData.text}
@@ -46,14 +85,32 @@ const CreatePostModal = ({ isOpen, setIsOpen, firstName, children }) => {
                 className="textarea h-24 textarea-ghost p-0 text-xl text-gray-900 dark:text-dark-txt placeholder:text-gray-800 focus:placeholder:text-gray-400 dark:bg-dark-second dark:focus:text-gray-200 dark:placeholder:text-gray-400 dark:focus:placeholder:text-gray-500"
                 placeholder={`Whats on your mind, ${capitalize(firstName)}?`}
               />
+              {showDropzone && (
+                <ImageDropzone files={files} setFiles={setFiles}>
+                  <ImagePreview files={files} setFiles={setFiles} />
+                </ImageDropzone>
+              )}
             </div>
           </div>
-          <div className="modal-action">
+          <div className="px-3 mt-2">
+            <div className="flex w-full p-2 border-[1px] border-gray-300 dark:border-dark-third rounded-lg">
+              <p className="flex-1 px-2 flex items-center font-semibold dark:text-dark-txt">
+                Add to your post
+              </p>
+              <button
+                type="button"
+                onClick={toggleDropzone}
+                aria-label="show dropzone"
+                className="bx bx-images text-green-500 hover:text-green-400 px-2 flex items-center text-3xl cursor-pointer"
+              />
+            </div>
+          </div>
+          <div className="modal-action mt-4 px-3">
             <button
               type="button"
-              htmlFor="my-modal-2"
+              onClick={handleSubmit}
               className="btn btn-block disabled:dark:text-gray-500 text-semibold capitalize text-base disabled:dark:bg-dark-third bg-btn-primary hover:bg-btn-primary-hover border-none"
-              disabled={!formData.text && true}
+              disabled={!formData.text && !files.length > 0 && true}
             >
               Post
             </button>
