f858ab011ba4a4588287f9218ee8fc73cc7cd071
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-26 01:42:49 +0100

    cleanup unused code

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -50,65 +50,6 @@ export const createPost = async (req, res) => {
             imageUrl,
         });
         if (createdPost) {
-            // const onImageUploadSuccess = async (fileName) => {
-            //     console.log(fileName, 'fileName');
-            //     try {
-            //         const addImageUrlToPost = await Post.findOneAndUpdate(
-            //             { _id: createdPost.id },
-            //             {
-            //                 $set: { imageUrl: `/images/${fileName}` },
-            //             }
-            //         );
-            //         // add post id to user posts array
-            //         const updated = await User.findOneAndUpdate(
-            //             { _id: userId },
-            //             {
-            //                 $push: { posts: createdPost.id },
-            //             }
-            //         );
-            //         if (updated && addImageUrlToPost) {
-            //             // get post data after update
-            //             const post = await Post.findById(createdPost.id);
-            //             return successRes(
-            //                 res,
-            //                 200,
-            //                 'ok',
-            //                 'post is created',
-            //                 post
-            //             );
-            //         }
-            //     } catch (error) {
-            //         console.log(error, 'error in onImageUploadSuccess');
-            //         return errorRes(
-            //             res,
-            //             500,
-            //             'something went wrong ...',
-            //             null,
-            //             null
-            //         );
-            //     }
-            //     return null;
-            // };
-            // const onImageUploadFail = (err) => {
-            //     console.log(err, 'onImageUploadFail func error');
-            //     return errorRes(
-            //         res,
-            //         500,
-            //         'failed to upload the image',
-            //         null,
-            //         null
-            //     );
-            // };
-            // if (req.file) {
-            //     uploadImg(
-            //         { name: 'post', id: createdPost.id },
-            //         req,
-            //         res,
-            //         onImageUploadSuccess,
-            //         onImageUploadFail
-            //     );
-            // }
-
             // add post id to user posts array
             const updated = await User.findOneAndUpdate(
                 { _id: userId },
