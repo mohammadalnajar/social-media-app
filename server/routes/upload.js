fcffe5565a7a57463e52e24b5b32311fbd04ab35
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-27 01:56:33 +0100

    added new route to save images in cloud

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import uploadImg from '../controllers/uploadImages.js';
+import { uploadImg, uploadImgCloud } from '../controllers/uploadImages.js';
 import isUserLoggedIn from '../middlewares/isLoggedIn.js';
 
 const router = express.Router();
 
 router.route('/').post(isUserLoggedIn, uploadImg);
+router.route('/cloud').post(isUserLoggedIn, uploadImgCloud);
 
 export default router;
