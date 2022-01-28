20fd81ba0de44871a0e89af5dada94eb5e60b007
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-28 02:46:25 +0100

    fix image base64

--- server/api/getRandomAvatar.js
+++ server/api/getRandomAvatar.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 const getAvatar = async (email) => {
     const res = await fetch(`https://robohash.org/${email}`);
     const data = await res.buffer();
-    const b64 = data.toString('base64');
+    const b64 = `data:image/jpeg;base64,${data.toString('base64')}`;
     return b64;
 };
 
@@ -12,3 +12,4 @@ export default getAvatar;
 // https://stackoverflow.com/questions/54099802/blob-to-base64-in-nodejs-without-filereader
 // https://stackoverflow.com/questions/34158497/convert-blob-data-to-raw-buffer-in-javascript-or-node
 // https://stackoverflow.com/questions/18650168/convert-blob-to-base64
+// https://github.com/cloudinary/cloudinary_npm/issues/177
