af5898e4a95e016626b4ef9131ee9a3955603e69
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-28 01:52:02 +0100

    questions + answers added

--- server/api/getRandomAvatar.js
+++ server/api/getRandomAvatar.js
@@ -8,3 +8,7 @@ const getAvatar = async (email) => {
 };
 
 export default getAvatar;
+
+// https://stackoverflow.com/questions/54099802/blob-to-base64-in-nodejs-without-filereader
+// https://stackoverflow.com/questions/34158497/convert-blob-data-to-raw-buffer-in-javascript-or-node
+// https://stackoverflow.com/questions/18650168/convert-blob-to-base64
