2cf8d3587476834b2e6a44d3f4e953cf250b2272
Hani al Sabti <h.sabti87@gmail.com>
2022-01-29 16:24:52 +0100

    added props to postAuthor

--- client/src/components/post/components/PostAuthor.jsx
+++ client/src/components/post/components/PostAuthor.jsx
@@ -1,20 +1,32 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { fetchUser } from '../../../utils/api';
+import timeElapsed from '../../../utils/timeElapsed';
 
-const PostAuthor = () => {
+const PostAuthor = ({ createdAt }) => {
+  const {
+    data: { data },
+  } = useQuery('fetchUser', fetchUser, {
+    retry: 0,
+  });
+  const fullName = `${data.firstName} ${data.lastName}`;
+  const timeAgo = timeElapsed(new Date(createdAt).getTime());
+
   return (
     <div className="flex items-center justify-between px-4 py-2">
       <div className="flex space-x-2 items-center">
         <div className="relative">
           <img
-            src="./assets/images/avt-2.jpg"
+            src={data.profileImageUrl}
             alt="Profile"
-            className="w-10 h-10 rounded-full"
+            className="w-10 h-10 rounded-full border-2 border-gray-300 dark:border-dark-hover"
           />
           <span className="bg-green-500 w-3 h-3 rounded-full absolute right-0 top-3/4 border-white border-2" />
         </div>
-        <div>
-          <div className="font-semibold">John Doe</div>
-          <span className="text-sm text-gray-500">38m</span>
+        <div className="flex flex-col leading-tight justify-center">
+          <div className="font-semibold">{fullName}</div>
+          <span className="text-sm text-gray-500">{timeAgo}</span>
         </div>
       </div>
       <div className="w-8 h-8 grid place-items-center text-xl text-gray-500 hover:bg-gray-200 dark:text-dark-txt dark:hover:bg-dark-third rounded-full cursor-pointer">
@@ -22,6 +34,10 @@ const PostAuthor = () => {
       </div>
     </div>
   );
+};
+
+PostAuthor.propTypes = {
+  createdAt: PropTypes.string.isRequired,
 };
 
 export default PostAuthor;
