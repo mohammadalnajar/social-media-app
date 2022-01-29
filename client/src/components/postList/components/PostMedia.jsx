4777235ab1affa12c70fcaa0070bf26ab0ddb741
Hani al Sabti <h.sabti87@gmail.com>
2022-01-29 13:56:44 +0100

    created a postMedia

---
+++ client/src/components/postList/components/PostMedia.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostMedia = () => {
+  return (
+    <div className="py-2">
+      <img src="./assets/images/post.png" alt="Post" />
+    </div>
+  );
+};
+
+export default PostMedia;
