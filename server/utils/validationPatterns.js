74aaa4cb33cd3696ff842fe6cd50545b92fae99b
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-01-29 01:25:41 +0100

    allow white spaces in first name and lastName

--- server/utils/validationPatterns.js
+++ server/utils/validationPatterns.js
@@ -1,6 +1,6 @@
 const patterns = {
     email: /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    name: { letters: /^[A-Za-z]+$/i, minName: /.{2,}$/ },
+    name: { letters: /^[A-Za-z ]+$/i, minName: /.{2,}$/ },
     password: {
         lowercaseLetter: /(?=.*?[a-z])/,
         uppercaseLetter: /^(?=.*?[A-Z])/,
