f08c12ad12debce023533ef3cff13c481c5aa413
Hani al Sabti <h.sabti87@gmail.com>
2022-01-29 13:56:36 +0100

    created a postContent

---
+++ client/src/components/postList/components/PostContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PostContent = () => {
+  return (
+    <div className="text-justify px-4 py-2">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, autem
+      earum cum ullam odio, molestias maxime aperiam in id aspernatur vel
+      ratione odit molestiae minus ipsa obcaecati quia! Doloribus, illum.
+    </div>
+  );
+};
+
+export default PostContent;
