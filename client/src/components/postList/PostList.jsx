6b6c3a732a3b01148cc28479a8f96efc2506d5e0
Hani al Sabti <h.sabti87@gmail.com>
2022-01-29 13:59:51 +0100

    removed leftover comments

--- client/src/components/postList/PostList.jsx
+++ client/src/components/postList/PostList.jsx
@@ -9,33 +9,14 @@ import PostStats from './components/PostStats';
 const PostList = () => {
   return (
     <div>
-      {/* <!-- POST --> */}
       <div className="shadow bg-white dark:bg-dark-second dark:text-dark-txt mt-4 rounded-lg">
-        {/* <!-- POST AUTHOR --> */}
         <PostAuthor />
-        {/* <!-- END POST AUTHOR --> */}
-
-        {/* <!-- POST CONTENT --> */}
         <PostContent />
-        {/* <!-- END POST CONTENT --> */}
-
-        {/* <!-- POST IMAGE --> */}
         <PostMedia />
-        {/* <!-- END POST IMAGE --> */}
-
-        {/* <!-- POST REACT --> */}
         <PostStats />
-        {/* <!-- END POST REACT --> */}
-
-        {/* <!-- POST ACTION --> */}
         <PostAction />
-        {/* <!-- END POST ACTION --> */}
-
-        {/* <!-- COMMENT FORM --> */}
         <PostComment />
-        {/* <!-- END COMMENT FORM --> */}
       </div>
-      {/* <!-- END POST --> */}
     </div>
   );
 };
