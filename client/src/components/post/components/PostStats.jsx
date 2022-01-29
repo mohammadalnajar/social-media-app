2bd46b2cc4f57d466d1093901abe60aab9ae8493
Hani al Sabti <h.sabti87@gmail.com>
2022-01-29 22:41:38 +0100

    added condition to postStats

--- client/src/components/post/components/PostStats.jsx
+++ client/src/components/post/components/PostStats.jsx
@@ -1,28 +1,37 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const PostStats = () => {
+const PostStats = ({ likes, dislikes, comments }) => {
   return (
     <div className="px-4 py-2">
       <div className="flex items-center justify-between">
-        <div className="flex flex-row-reverse items-center">
-          <span className="ml-2 text-gray-500 dark:text-dark-txt">999</span>
-          <span className="rounded-full grid place-items-center text-2xl -ml-1 text-red-800">
-            <i className="bx bxs-angry" />
-          </span>
-          <span className="rounded-full grid place-items-center text-2xl -ml-1 text-red-500">
-            <i className="bx bxs-heart-circle" />
-          </span>
-          <span className="rounded-full grid place-items-center text-2xl -ml-1 text-yellow-500">
-            <i className="bx bx-happy-alt" />
-          </span>
+        <div className="flex flex-row items-center">
+          {likes.length > 0 ? (
+            <span className="rounded-full grid place-items-center text-2xl mr-2">
+              <i className="bx bx-like" /> {likes.length}
+            </span>
+          ) : null}
+
+          {dislikes.length > 0 ? (
+            <span className="rounded-full grid place-items-center text-2xl">
+              <i className="bx bx-dislike" /> {dislikes.length}
+            </span>
+          ) : null}
         </div>
         <div className="text-gray-500 dark:text-dark-txt">
-          <span>90 comments</span>
-          <span>66 Shares</span>
+          <span>
+            {comments.length > 0 ? `${comments.length} comments` : null}
+          </span>
         </div>
       </div>
     </div>
   );
+};
+
+PostStats.propTypes = {
+  likes: PropTypes.arrayOf(PropTypes.object).isRequired,
+  dislikes: PropTypes.arrayOf(PropTypes.object).isRequired,
+  comments: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default PostStats;
