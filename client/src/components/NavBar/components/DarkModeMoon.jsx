b6170fdc15084949b601a7005ae576ef7e2c656a
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-02 12:58:47 +0100

    built a darkmode switcher with animation and icons

--- client/src/components/NavBar/components/DarkModeMoon.jsx
+++ client/src/components/NavBar/components/DarkModeMoon.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import useDarkMode from '../../../hooks/useDarkMode';
 import useLocalStorage from '../../../hooks/useLocalStorage';
@@ -6,38 +6,38 @@ import '../../../index.css';
 
 const DarkModeMoon = ({ extraClasses }) => {
   const [storedValue, setValue] = useLocalStorage('darkMode');
+  const [checked, setChecked] = useState(() => {
+    if (storedValue === 'dark') {
+      return false;
+    }
+    return true;
+  });
   const [colorTheme, setTheme] = useDarkMode();
-
   const darkModeToggle = () => {
+    setChecked(!checked);
     setValue(storedValue === 'dark' ? 'light' : 'dark');
     setTheme(colorTheme);
   };
 
   return (
-    <main id="main night">
-      <div className="container">
-        <div className="switcher-wrapper">
-          <p className="mode light">Light</p>
-          <div id="switcher">
-            <div className="star star1" />
-            <div className="star star2" />
-            <div className="star star3" />
-            <div className="round-btn" role="button">
-              <div className="moon-mode" />
-            </div>
-          </div>
-          <p className="mode dark">Dark</p>
-        </div>
-      </div>
-    </main>
-    // <button
-    //   type="button"
-    //   onClick={darkModeToggle}
-    //   className={`${extraClasses} text-2xl grid place-items-center bg-gray-200 dark:bg-dark-third rounded-full w-10 h-10 cursor-pointer hover:bg-gray-300 dark:text-dark-txt`}
-    //   id="dark-mode-toggle-mb"
-    // >
-    //   <i className="bx bxs-moon" />
-    // </button>
+    <div className={`${extraClasses} container m-2`}>
+      <button
+        type="button"
+        id="switch"
+        onClick={() => {
+          darkModeToggle();
+        }}
+        className="switch"
+      >
+        <input
+          type="checkbox"
+          onChange={() => {}}
+          checked={checked}
+          id="slider"
+        />
+        <span className="slider round" />
+      </button>
+    </div>
   );
 };
 DarkModeMoon.propTypes = {
