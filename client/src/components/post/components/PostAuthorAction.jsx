abe99fc30c46c31b29dc210bc919dd52b25c8292
Hani al Sabti <h.sabti87@gmail.com>
2022-02-03 18:44:04 +0100

    added darkmode to dropdown

--- client/src/components/post/components/PostAuthorAction.jsx
+++ client/src/components/post/components/PostAuthorAction.jsx
@@ -9,14 +9,14 @@ const PostAuthorAction = () => {
       >
         <i className="bx bx-dots-horizontal-rounded" />
       </button>
-      <ul className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
-        <li>
+      <ul className="p-2 shadow menu dropdown-content bg-base-100 dark:bg-dark-third rounded-box w-52">
+        <li className="dark:hover:bg-dark-hover dark:text-gray-100">
           <button type="button">Item 1</button>
         </li>
-        <li>
+        <li className="dark:hover:bg-dark-hover dark:text-gray-100">
           <button type="button">Item 2</button>
         </li>
-        <li>
+        <li className="dark:hover:bg-dark-hover dark:text-gray-100">
           <button type="button">Item 3</button>
         </li>
       </ul>
