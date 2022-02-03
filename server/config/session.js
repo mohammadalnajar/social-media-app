78c619c4afe45f0551f2cf963cd1a01a86bd3957
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-03 08:20:02 +0100

    set cookies secure true

--- server/config/session.js
+++ server/config/session.js
@@ -7,7 +7,7 @@ const sessionConfig = () => {
             mongoUrl: process.env.MONGODB_URL,
         }),
         secret: 'DfSfdSsDdsFe3##$@DVD3385^$@&$@#',
-        cookie: { maxAge: 10 * 60 * 1000 }, // 300000ms = 5 min / 900000ms = 15 min
+        cookie: { maxAge: 10 * 60 * 1000, secure: true, sameSite: 'none' }, // 300000ms = 5 min / 900000ms = 15 min
         saveUninitialized: false,
     });
     return sessionConfigObj;
