640523a4e0381aec2c7b542f17e97fe5e1b682fa
Hani al Sabti <h.sabti87@gmail.com>
2022-02-04 13:08:11 +0100

    created deletePost queryFn

--- client/src/components/post/api.js
+++ client/src/components/post/api.js
@@ -1,14 +1,23 @@
 import { handleApiResponse } from '../../utils/api';
-import { FEED_URL, POST_URL } from '../../utils/constants';
+import { FEED_URL, POST_URL, SERVER_URL } from '../../utils/constants';
 
 export const getUserPosts = async () => {
-  return fetch(`${process.env.REACT_APP_SERVER_URL}${POST_URL}`, {
+  return fetch(`${SERVER_URL}${POST_URL}`, {
     credentials: 'include',
   }).then(handleApiResponse);
 };
 
 export const getFeedPosts = async () => {
-  return fetch(`${process.env.REACT_APP_SERVER_URL}${POST_URL}${FEED_URL}`, {
+  return fetch(`${SERVER_URL}${POST_URL}${FEED_URL}`, {
     credentials: 'include',
+  }).then(handleApiResponse);
+};
+
+export const deletePost = async (data) => {
+  return fetch(`${SERVER_URL}${POST_URL}`, {
+    method: 'DELETE',
+    credentials: 'include',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(data),
   }).then(handleApiResponse);
 };
