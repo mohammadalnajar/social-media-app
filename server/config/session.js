524624d172d285dc9a2105f4c5081e76831469a1
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-05 01:54:49 +0100

    set samestie and secure

--- server/config/session.js
+++ server/config/session.js
@@ -7,7 +7,11 @@ const sessionConfig = () => {
             mongoUrl: process.env.MONGODB_URL,
         }),
         secret: 'DfSfdSsDdsFe3##$@DVD3385^$@&$@#',
-        cookie: { maxAge: 10 * 60 * 1000 }, // 300000ms = 5 min / 900000ms = 15 min
+        cookie: {
+            maxAge: 10 * 60 * 1000,
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : false,
+            secure: process.env.NODE_ENV === 'production' && true,
+        }, // 300000ms = 5 min / 900000ms = 15 min
         saveUninitialized: false,
     });
     return sessionConfigObj;
