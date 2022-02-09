8cbfcf6ae788a9dd87366f648d566167c040cf77
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-09 12:53:52 +0100

    icons added for each dropdown item

--- client/src/components/NavBar/components/optionsMenu/dropdown/DropDownItem.jsx
+++ client/src/components/NavBar/components/optionsMenu/dropdown/DropDownItem.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DropDownItem = ({ title, onClick }) => {
+const DropDownItem = ({ title, onClick, children }) => {
   return (
-    <button
-      onClick={onClick}
-      className="dark:hover:bg-dark-hover rounded-box"
-      type="button"
-    >
-      {title}
-    </button>
+    <li>
+      <button
+        onClick={onClick}
+        className="dark:hover:bg-dark-hover rounded-box"
+        type="button"
+      >
+        <div className="mr-3">{children}</div>
+        <div>{title}</div>
+      </button>
+    </li>
   );
 };
 
 DropDownItem.propTypes = {
   title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
   onClick: PropTypes.func,
 };
 DropDownItem.defaultProps = {
