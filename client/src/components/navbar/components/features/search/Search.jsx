95e4211b8b536c575e550bbe7a4cc8cc4821e5ec
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-09 23:46:26 +0100

    prepare useEffect func to fetch users onKeypress

--- client/src/components/navbar/components/features/search/Search.jsx
+++ client/src/components/navbar/components/features/search/Search.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SearchField from './components/SearchField';
+import useToggleOpen from '../../../../../hooks/useToggleOpen';
+import SearchMenu from './components/SearchMenu';
+import useForm from '../../../../../hooks/useForm';
 
 const Search = () => {
-  return <SearchField />;
+  const { isOpen: isModalOpen, toggle: toggleModal } = useToggleOpen({
+    initialState: false,
+  });
+  const { formData, handleInputChange, reset } = useForm({ search: '' });
+
+  useEffect(() => {
+    // fetch users onKey press comes here
+    console.log(formData.search);
+  }, [formData.search]);
+
+  return (
+    <>
+      <SearchField
+        toggleModal={toggleModal}
+        handleInputChange={handleInputChange}
+      />
+      {isModalOpen && <SearchMenu searchText={formData.search} />}
+    </>
+  );
 };
 
 export default Search;
