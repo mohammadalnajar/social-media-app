0a7403c6996b6313c4b8e7f906533271f414cbf4
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-09 16:08:49 +0100

    messenger icon + messengar component refactored

--- client/src/components/navbar/components/features/messenger/Messenger.jsx
+++ client/src/components/navbar/components/features/messenger/Messenger.jsx
@@ -1,37 +1,21 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useRef } from 'react';
+import useToggleOpen from '../../../../../hooks/useToggleOpen';
 import MessengerIcon from './components/MessengerIcon';
 import MessengerModal from './components/MessengerModal';
-import capitalize from '../../../../../utils/helpers';
 
-const Messenger = ({ extraClasses }) => {
-  const [showModal, setShowModal] = useState(false);
-  const [openTooltip, setOpenTooltip] = useState(false);
+const Messenger = () => {
+  const ref = useRef();
+  const { isOpen: showModal, toggle: toggleModal } = useToggleOpen({
+    initialState: false,
+    ref,
+  });
+
   return (
     <div>
-      <div
-        data-tip="Messenger"
-        onMouseEnter={() => {
-          setOpenTooltip(true);
-        }}
-        onMouseLeave={() => {
-          setOpenTooltip(false);
-        }}
-        className={`${extraClasses} tooltip ${
-          openTooltip && 'tooltip-open '
-        } tooltip-bottom text-2xl grid place-items-center bg-gray-200 dark:bg-dark-third rounded-full w-10 h-10 cursor-pointer hover:bg-gray-300 dark:text-dark-txt`}
-      >
-        <MessengerIcon setShowModal={setShowModal} />
-      </div>
-      {showModal && (
-        <div className="absolute top-20 right-6">
-          <MessengerModal />
-        </div>
-      )}
+      <MessengerIcon toggleModal={toggleModal} />
+      {showModal && <MessengerModal myRef={ref} />}
     </div>
   );
 };
-Messenger.propTypes = {
-  extraClasses: PropTypes.string.isRequired,
-};
+
 export default Messenger;
--- client/src/components/navbar/components/features/messenger/components/MessengerIcon.jsx
+++ client/src/components/navbar/components/features/messenger/components/MessengerIcon.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import useToggleOpen from '../../../../../../hooks/useToggleOpen';
 
-const MessengerIcon = ({ setShowModal }) => {
-  const toggleMessengerModal = () => {
-    setShowModal((current) => {
-      return !current;
-    });
-  };
+const MessengerIcon = ({ toggleModal }) => {
+  const { isOpen: isOpenTooltip, toggle: toggleTooltip } = useToggleOpen({
+    initialState: false,
+  });
+
   return (
-    <button type="button" onClick={toggleMessengerModal}>
+    <button
+      data-tip="Messenger"
+      onMouseEnter={toggleTooltip}
+      onMouseLeave={toggleTooltip}
+      type="button"
+      onClick={toggleModal}
+      className={`tooltip ${
+        isOpenTooltip && 'tooltip-open'
+      } tooltip-bottom text-2xl grid place-items-center bg-gray-200 dark:bg-dark-third rounded-full w-10 h-10 cursor-pointer hover:bg-gray-300 dark:text-dark-txt`}
+    >
       <i className="bx bxl-messenger" />
+      <span className="text-xs absolute top-0 right-0 bg-red-500 text-white font-semibold rounded-full px-1 text-center">
+        4
+      </span>
     </button>
   );
 };
 MessengerIcon.propTypes = {
-  setShowModal: PropTypes.func.isRequired,
+  toggleModal: PropTypes.func.isRequired,
 };
 export default MessengerIcon;
