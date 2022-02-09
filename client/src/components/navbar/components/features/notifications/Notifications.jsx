21344d084a4a48a53988f411d1d61a032fc82401
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-09 14:38:58 +0100

    useToggleOpen used in notification component

--- client/src/components/navbar/components/features/notifications/Notifications.jsx
+++ client/src/components/navbar/components/features/notifications/Notifications.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from 'react';
 import NotificationsIcon from './components/NotificationsIcon';
 import NotificationsMenu from './components/NotificationsMenu';
+import useToggleOpen from '../../../../../hooks/useToggleOpen';
 
 const Notifications = () => {
   const [showModal, setShowModal] = useState(false);
-  const [openTooltip, setOpenTooltip] = useState(false);
+  const { isOpen: isOpenTooltip, toggle: toggleTooltip } = useToggleOpen({
+    initialState: false,
+  });
 
   return (
     <div
       data-tip="Notifications"
-      onMouseEnter={() => {
-        setOpenTooltip(true);
-      }}
-      onMouseLeave={() => {
-        setOpenTooltip(false);
-      }}
-      className={` tooltip ${openTooltip && 'tooltip-open '} tooltip-bottom`}
+      onMouseEnter={toggleTooltip}
+      onMouseLeave={toggleTooltip}
+      className={` tooltip ${isOpenTooltip && 'tooltip-open '} tooltip-bottom`}
     >
       <NotificationsIcon setShowModal={setShowModal} />
       {showModal && (
