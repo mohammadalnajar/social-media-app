8087c29d3bc931eb9439109f0a1a53ac7f912851
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-11 08:36:39 +0100

    created like & dislike a post requests functions

--- client/src/components/post/api.js
+++ client/src/components/post/api.js
@@ -1,5 +1,11 @@
 import { handleApiResponse } from '../../utils/api';
-import { FEED_URL, POST_URL, SERVER_URL } from '../../utils/constants';
+import {
+  FEED_URL,
+  POST_URL,
+  SERVER_URL,
+  LIKE_URL,
+  DISLIKE_URL,
+} from '../../utils/constants';
 
 export const getUserPosts = async () => {
   return fetch(`${SERVER_URL}${POST_URL}`, {
@@ -16,6 +22,26 @@ export const getFeedPosts = async () => {
 export const deletePost = async (data) => {
   return fetch(`${SERVER_URL}${POST_URL}`, {
     method: 'DELETE',
+    credentials: 'include',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(data),
+  }).then(handleApiResponse);
+};
+
+export const likePost = async (data) => {
+  const { postId } = data;
+  return fetch(`${SERVER_URL}${POST_URL}${LIKE_URL}${postId}`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(data),
+  }).then(handleApiResponse);
+};
+
+export const dislikePost = async (data) => {
+  const { postId } = data;
+  return fetch(`${SERVER_URL}${POST_URL}${DISLIKE_URL}${postId}`, {
+    method: 'POST',
     credentials: 'include',
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify(data),
