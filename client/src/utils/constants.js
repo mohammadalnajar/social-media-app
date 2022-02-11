067129f4393267175405d28ab63c922e47c58177
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-11 08:36:16 +0100

    add like and dislike url to constants

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -4,3 +4,5 @@ export const IMG_URL = 'api/uploadImage/';
 export const IMG_CLOUD_URL = 'api/uploadImage/cloud';
 export const POST_URL = 'api/posts/';
 export const FEED_URL = 'users';
+export const LIKE_URL = 'like/';
+export const DISLIKE_URL = 'dislike/';
