c1f8bfb24936221beb4c98174864c6e7ed65b41f
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-12 20:45:59 +0100

    remove compnents and put children

--- client/src/components/post/components/PostAction.jsx
+++ client/src/components/post/components/PostAction.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import Comment from './postActions/Comment';
-import Dislike from './postActions/Dislike';
-import Like from './postActions/Like';
-import Share from './postActions/Share';
+import PropTypes from 'prop-types';
 
-const PostAction = () => {
+const PostAction = ({ children }) => {
+  // const res = useQuery([`getLikes-${postId}`, { postId }], getLikes, {
+  //   staleTime: 10 * 60 * 1000,
+  //   refetchInterval: 10 * 60 * 1000,
+  // });
+  // console.log(res.data);
   return (
     <div className="py-2 px-4">
       <div className="border border-gray-200 dark:border-dark-third border-l-0 border-r-0 py-1">
-        <div className="flex space-x-2">
-          <Like />
-          <Dislike />
-          <Comment />
-          <Share />
-        </div>
+        <div className="flex space-x-2">{children}</div>
       </div>
     </div>
   );
 };
-
+PostAction.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 export default PostAction;
