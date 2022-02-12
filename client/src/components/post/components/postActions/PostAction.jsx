7ce7f03b8b7ebb7d41d85ff2a8a72b32cc8122b5
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-12 21:38:46 +0100

    postAction + postStats place changed

--- client/src/components/post/components/postActions/PostAction.jsx
+++ client/src/components/post/components/postActions/PostAction.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const PostAction = ({ children }) => {
-  // const res = useQuery([`getLikes-${postId}`, { postId }], getLikes, {
-  //   staleTime: 10 * 60 * 1000,
-  //   refetchInterval: 10 * 60 * 1000,
-  // });
-  // console.log(res.data);
   return (
     <div className="py-2 px-4">
       <div className="border border-gray-200 dark:border-dark-third border-l-0 border-r-0 py-1">
