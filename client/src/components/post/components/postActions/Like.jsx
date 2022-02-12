b6c4e9d86b662c9deb1a6d33f1df9a4660ec5bf9
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-12 20:48:51 +0100

    handle click on like

--- client/src/components/post/components/postActions/Like.jsx
+++ client/src/components/post/components/postActions/Like.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import useLike from '../../hooks/useLike';
 
-const Like = () => {
+const Like = ({ postId, likes }) => {
+  const { likeOrUnlikePost, checked: userLikePost } = useLike({
+    likes,
+  });
+
+  const handleClick = () => {
+    const data = {
+      postId,
+      like: !userLikePost,
+    };
+
+    likeOrUnlikePost.mutate(data);
+  };
   return (
-    <div className="w-1/3 flex space-x-2 justify-center items-center hover:bg-gray-100 dark:hover:bg-dark-third text-xl py-2 rounded-lg cursor-pointer text-gray-500 dark:text-dark-txt">
+    <button
+      onClick={handleClick}
+      type="button"
+      className={`w-1/3 flex space-x-2 justify-center items-center hover:bg-gray-100 dark:hover:bg-dark-third text-xl py-2 rounded-lg cursor-pointer ${
+        userLikePost ? 'text-blue-500' : 'text-gray-500 dark:text-dark-txt'
+      } `}
+    >
       <i className="bx bx-like" />
       <span className="text-sm font-semibold">Like</span>
-    </div>
+    </button>
   );
 };
+Like.propTypes = {
+  postId: PropTypes.string.isRequired,
+  likes: PropTypes.arrayOf(PropTypes.object),
+};
 
+Like.defaultProps = {
+  likes: [{}],
+};
 export default Like;
