100ca916ef4b168e64fa56c59495aabccede3ea2
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-13 23:59:02 +0100

    animate search field on md screens

--- client/src/components/navbar/components/features/search/components/SearchField.jsx
+++ client/src/components/navbar/components/features/search/components/SearchField.jsx
@@ -1,28 +1,37 @@
 import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import useToggle from '../../../../../../hooks/useToggle';
+import useWindowSize from '../../../../../../hooks/useWindowSize';
 
 const SearchField = ({ isModalOpen, toggleModal, handleInputChange }) => {
+  const screenSize = useWindowSize();
   const ref = useRef();
   const [isFieldOpen, toggleField] = useToggle({
     ref,
+    initialState: '',
   });
+
+  const mdScreen = screenSize.width <= 1200 && true;
 
   return (
     <div
-      className={`relative xl:w-64 bg-gray-100 dark:bg-dark-third px-2 py-2 ${
-        !isFieldOpen ? 'w-15 md:w-15' : 'w-65'
-      }  h-10 md:h-10 xl:pl-3 xl:pr-8 rounded-full flex items-center justify-center cursor-pointer`}
+      ref={ref}
+      className={`bg-gray-100 dark:bg-dark-third px-2 py-2 ${
+        mdScreen && isFieldOpen !== '' && !isFieldOpen
+          ? 'animate-widthRed'
+          : 'xl:w-0'
+      } ${
+        mdScreen && isFieldOpen && 'animate-widthEx'
+      } xl:w-64 h-10 md:h-10 xl:pl-3 xl:pr-8 rounded-full flex items-center justify-center cursor-pointer transition-all duration-75 `}
     >
-      <button type="button" onClick={toggleField}>
+      <button type="button" className="w-full h-full" onClick={toggleField}>
         <i
-          className={`bx bx-search-alt-2 text-xl ${
-            isFieldOpen && 'mr-2'
-          } xl:mr-2 dark:text-dark-txt ${isModalOpen && 'hidden'}`}
+          className={`bx  bx-search-alt-2 text-xl  ${
+            mdScreen && isFieldOpen && 'mr-2'
+          }  dark:text-dark-txt ${isModalOpen && 'hidden'}`}
         />
       </button>
       <input
-        ref={ref}
         type="text"
         name="search"
         autoComplete="off"
@@ -31,8 +40,10 @@ const SearchField = ({ isModalOpen, toggleModal, handleInputChange }) => {
         onChange={handleInputChange}
         placeholder="Search Facebook"
         className={`outline-none bg-transparent dark:text-white ${
-          !isFieldOpen && 'hidden'
-        }  xl:inline-block`}
+          mdScreen && isFieldOpen !== '' && !isFieldOpen
+            ? 'animate-widthRedBar'
+            : 'w-0'
+        } ${mdScreen && isFieldOpen && 'animate-widthExBar'} xl:w-64`}
       />
     </div>
   );
