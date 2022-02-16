d843e0c8d9a0aff782cc932a43c25cecb6216a46
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-16 09:22:01 +0100

    fix tooltip issue rendering + show only on large screens

--- client/src/components/navbar/components/features/notifications/components/NotificationsIcon.jsx
+++ client/src/components/navbar/components/features/notifications/components/NotificationsIcon.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import useToggle from '../../../../../../hooks/useToggle';
+import useWindowSize from '../../../../../../hooks/useWindowSize';
 
 const NotificationsIcon = ({ toggleModal, isModalOpen }) => {
-  const [isOpenTooltip, toggleTooltip] = useToggle({});
+  const screenSize = useWindowSize();
+  const lgScreen = screenSize.width >= 1024 && true;
 
   return (
     <button
       data-tip="Notifications"
-      onMouseEnter={toggleTooltip}
-      onMouseLeave={toggleTooltip}
       type="button"
       onClick={toggleModal}
-      className={` ${isOpenTooltip && 'tooltip-open '} ${
+      className={` ${lgScreen && 'tooltip tooltip-bottom'}  ${
         isModalOpen
           ? 'dark:bg-dark-bg-modal-open dark:text-dark-text-modal-open bg-bg-modal-open text-text-modal-open'
           : 'dark:bg-dark-third dark:text-dark-txt'
-      }  tooltip-bottom text-xl grid place-items-center bg-gray-200  rounded-full mx-1 p-2.5 cursor-pointer dark:hover:bg-gray-600 hover:bg-gray-300 relative tooltip`}
+      }   text-xl grid place-items-center bg-gray-200  rounded-full mx-1 p-2.5 cursor-pointer dark:hover:bg-gray-600 hover:bg-gray-300 relative`}
     >
       <i className="bx bxs-bell" />
       <span className="text-xs absolute top-0 right-0 bg-red-500 text-white font-semibold rounded-full px-1 text-center">
