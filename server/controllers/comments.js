c5c9313a5f3b6947b8518624fc1688fba289ea18
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-18 22:50:06 +0100

    init comments controllers

---
+++ server/controllers/comments.js
@@ -0,0 +1,12 @@
+export const getPostComments = async (req, res) => {
+    res.send('get comments');
+};
+export const createComment = async (req, res) => {
+    res.send('create comment');
+};
+export const updateComment = async (req, res) => {
+    res.send('update comment');
+};
+export const deleteComment = async (req, res) => {
+    res.send('delete comment');
+};
