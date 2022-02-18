a0d2383cf5f2a550cc076d86203cde3e15d4f768
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-18 23:58:48 +0100

    comment schema created

---
+++ server/models/Comment.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const CommentSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+    },
+
+    likes: [{ type: String }],
+    dislikes: [{ type: String }],
+    comments: [{ type: String }],
+    photos: { type: Array },
+    imageUrl: { type: String },
+    imagePublicId: { type: String },
+    userId: { type: String, required: true },
+});
+
+const Comment = mongoose.model('comments', CommentSchema);
+
+export default Comment;
