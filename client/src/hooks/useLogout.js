2f4ef05404625b7aebe8699c1765a3b027e776bb
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-20 23:57:02 +0100

    remove cache when lpogout

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,12 +1,14 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import { logoutUser } from '../utils/api';
 
 const useLogout = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const logout = useMutation(logoutUser, {
     onSuccess: (data) => {
       if (data?.status === 'ok') navigate('/');
+      queryClient.removeQueries('fetchUser', { exact: true });
     },
     onError: (err) => {
       if (err.status === 'not authenticated') navigate('/');
