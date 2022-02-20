a1ae38b536ab116c79d06982bd526dfafaa4e318
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-20 20:43:58 +0100

    add edit funtionality to usecomment hook

--- client/src/components/post/hooks/useComment.js
+++ client/src/components/post/hooks/useComment.js
@@ -1,13 +1,15 @@
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import useForm from '../../../hooks/useForm';
-import { createComment } from '../api';
+import { createComment, updateComment } from '../api';
 
-const useComment = ({ postId }) => {
+const useComment = ({ postId, defaultTextVal }) => {
   const {
     data: { data: userData },
   } = useQuery('fetchUser');
 
-  const { formData, handleInputChange, reset } = useForm({ text: '' });
+  const { formData, handleInputChange, reset } = useForm({
+    text: defaultTextVal,
+  });
 
   const queryClient = useQueryClient();
 
@@ -22,7 +24,21 @@ const useComment = ({ postId }) => {
     },
   });
 
-  return { userData, postComment, formData, handleInputChange, reset };
+  const editComment = useMutation(updateComment, {
+    onSuccess: () => {
+      invalidateCommentsQuery();
+      reset();
+    },
+  });
+
+  return {
+    userData,
+    postComment,
+    editComment,
+    formData,
+    handleInputChange,
+    reset,
+  };
 };
 
 export default useComment;
