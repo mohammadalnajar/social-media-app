d248da3efb176d41dec2d6168bdc80ae5841bd9d
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-20 10:12:15 +0100

    built commetns services to get data for post

---
+++ server/services/comments.js
@@ -0,0 +1,47 @@
+/* eslint-disable node/no-unsupported-features/es-builtins */
+import Comment from '../models/Comment.js';
+import getUserData from './users.js';
+
+const commentServices = {
+    async getPostComments(commentsIds) {
+        try {
+            // get all comments objs
+            const commentsArr = await Promise.allSettled(
+                commentsIds.map(async (commentId) => {
+                    const comment = await Comment.findById(commentId);
+                    if (comment) return comment;
+                    return null;
+                })
+            );
+            const comments = commentsArr.map((obj) => obj.value);
+
+            // get complete comments data with user data
+            const commentsDataArr = await Promise.allSettled(
+                comments.map(async (comment) => {
+                    const commentData = await this.getCommentData(comment);
+                    return commentData;
+                })
+            );
+
+            const commentsData = commentsDataArr.map((obj) => obj.value);
+            return { commentsData };
+        } catch (error) {
+            console.log(error, 'error in comment service getPostComments ...');
+            throw new Error(error);
+        }
+    },
+
+    async getCommentData(comment) {
+        const { _id, firstName, lastName } = await getUserData(
+            comment.userId,
+            'get user data in comment service getCommentData' // this is the context to inform where this service get used
+        );
+        const { userId, ...rest } = comment._doc;
+        return {
+            ...rest, // rest data from comment obj
+            userData: { userId: _id, firstName, lastName },
+        };
+    },
+};
+
+export default commentServices;
