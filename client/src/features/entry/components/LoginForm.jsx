a027d04e3395953bdd26e2abac58f13fe39f32e0
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-02-26 01:14:41 +0100

    used pass eye in login form

--- client/src/features/entry/components/LoginForm.jsx
+++ client/src/features/entry/components/LoginForm.jsx
@@ -3,10 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import ErrorAlert from '../../../components/ErrorAlert';
 import { useAuth } from '../../../context/authContext';
 import useForm from '../../../hooks/useForm';
+import usePassEye from '../../../hooks/usePassEye';
 import FormButton from './FormButton';
 
 const LoginForm = () => {
   const navigate = useNavigate();
+  const { passShowEye, passHideEye, clickEye } = usePassEye();
   const { formData, handleInputChange, reset } = useForm({
     email: '',
     password: '',
@@ -52,7 +54,7 @@ const LoginForm = () => {
       </div>
 
       <div className="mt-4">
-        <div className="flex justify-between">
+        <div className="flex justify-between ">
           <span className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
             Password
           </span>
@@ -63,15 +65,33 @@ const LoginForm = () => {
             Forget Password?
           </button>
         </div>
-        <input
-          name="password"
-          className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-          type="password"
-          autoComplete="password"
-          value={formData.password}
-          onChange={handleInputChange}
-          required
-        />
+        <div className="relative">
+          <input
+            name="password"
+            className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 "
+            type={passShowEye ? 'text' : 'password'}
+            autoComplete="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+          {passHideEye && (
+            <button type="button" onClick={clickEye}>
+              <i
+                className="fa-solid fa-eye-slash absolute top-3 right-3 text-gray-700 cursor-pointer dark:hover:text-gray-500
+  dark:text-gray-300"
+              />
+            </button>
+          )}
+          {passShowEye && (
+            <button type="button" onClick={clickEye}>
+              <i
+                className="fa-solid fa-eye absolute top-3 right-3 text-gray-700 cursor-pointer dark:hover:text-gray-500
+dark:text-gray-300"
+              />
+            </button>
+          )}
+        </div>
         {login?.error?.error && (
           <ErrorAlert errorMessage={login.error.error} duration={5000} />
         )}
