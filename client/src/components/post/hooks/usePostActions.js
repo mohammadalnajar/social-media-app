48a6f595a664bd8d34f3a2fcad421766b21286bb
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-03-04 08:32:29 +0100

    make only one query to get all post stats

--- client/src/components/post/hooks/usePostActions.js
+++ client/src/components/post/hooks/usePostActions.js
@@ -1,28 +1,10 @@
 import { useQuery } from 'react-query';
-import { getDislikes, getLikes, getPostComments } from '../api';
+import { getPostStats } from '../api';
 
 const usePostActions = ({ postId }) => {
-  const { data: { data: { likes } = {} } = {} } = useQuery(
-    [`getLikes-${postId}`, { postId }],
-    getLikes,
-    {
-      staleTime: 2 * 60 * 1000,
-      refetchInterval: 2 * 60 * 1000,
-    }
-  );
-
-  const { data: { data: { dislikes } = {} } = {} } = useQuery(
-    [`getDislikes-${postId}`, { postId }],
-    getDislikes,
-    {
-      staleTime: 2 * 60 * 1000,
-      refetchInterval: 2 * 60 * 1000,
-    }
-  );
-
-  const { data: { data: { comments } = {} } = {} } = useQuery(
-    [`getComments-${postId}`, { postId }],
-    getPostComments,
+  const { data: { data: { comments, likes, dislikes } = {} } = {} } = useQuery(
+    [`getPostStats-${postId}`, { postId }],
+    getPostStats,
     {
       staleTime: 2 * 60 * 1000,
       refetchInterval: 2 * 60 * 1000,
