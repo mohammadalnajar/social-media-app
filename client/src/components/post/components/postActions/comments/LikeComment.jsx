86d9482fd883967dec6fcd8dba8a5d8e4bb3b85f
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-03-12 00:09:05 +0100

    like comment btn 1st stage done

--- client/src/components/post/components/postActions/comments/LikeComment.jsx
+++ client/src/components/post/components/postActions/comments/LikeComment.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import useComment from '../../../hooks/useComment';
 
-const LikeComment = () => {
+const LikeComment = ({ commentId, likes }) => {
+  const { checked } = useComment({ likes });
   return (
-    <span className="font-semibold cursor-pointer hover:underline dark:hover:text-white">
+    <button
+      type="submit"
+      onClick={() => console.log(commentId, likes, checked)}
+      className={`${
+        checked ? 'dark:text-blue-500' : 'dark:text-white'
+      } font-semibold cursor-pointer hover:underline `}
+    >
       Like
-    </span>
+    </button>
   );
 };
 
+LikeComment.propTypes = {
+  commentId: PropTypes.string.isRequired,
+  likes: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 export default LikeComment;
