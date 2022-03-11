005df67fe8dc2360df93a0158ddef15d6215e6f6
Mohammad-Al-Najar <malnajjar16@gmail.com>
2022-03-12 00:07:58 +0100

    check if user likes  a comment

--- client/src/components/post/hooks/useComment.js
+++ client/src/components/post/hooks/useComment.js
@@ -1,16 +1,40 @@
+/* eslint-disable no-underscore-dangle */
+import { useState } from 'react';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import useForm from '../../../hooks/useForm';
 import useLogout from '../../../hooks/useLogout';
 import { createComment, deleteComment, updateComment } from '../api';
 
-const useComment = ({ postId, defaultTextVal, close }) => {
+const useComment = ({ postId, defaultTextVal, close, likes }) => {
   const {
     data: { data: userData },
   } = useQuery('fetchUser');
   const { navigateToLogin } = useLogout();
+
   const { formData, handleInputChange, reset } = useForm({
     text: defaultTextVal,
   });
+
+  const checkUserLikedComment = (arr) => {
+    let check = false;
+    arr?.forEach((action) => {
+      if (action?.userId === userData._id) {
+        check = true;
+      }
+    });
+    return check;
+  };
+
+  const [checked, setChecked] = useState(() => {
+    if (likes) {
+      return checkUserLikedComment(likes);
+    }
+    return false;
+  });
+
+  const toggleChecked = () => {
+    setChecked(!checked);
+  };
 
   const queryClient = useQueryClient();
 
@@ -57,6 +81,8 @@ const useComment = ({ postId, defaultTextVal, close }) => {
     formData,
     handleInputChange,
     reset,
+    checked,
+    toggleChecked,
   };
 };
 
