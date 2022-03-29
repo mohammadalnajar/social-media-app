bc5b39697ab0ff527f1c12cfe9cbea27024d1771
Mohammad Al Najar <44784702+mohammadalnajar@users.noreply.github.com>
2022-03-30 00:22:16 +0200

    change logo on small screens

--- client/src/components/navbar/components/Logo.jsx
+++ client/src/components/navbar/components/Logo.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Logo = () => {
   return (
     <>
-      <button type="button" href="#" className="mr-2 hidden md:inline-block">
+      <button type="button" href="#" className="mr-2 inline-block md:hidden">
         <img
           src="./assets/images/fb-logo.png"
           alt="Facebook logo"
-          className="w-24 sm:w-10 lg:w-10 h-auto"
+          className="w-10 h-auto"
         />
       </button>
-      <button type="button" href="#" className="inline-block md:hidden">
+      <button type="button" href="#" className="mr-2 hidden md:inline-block">
         <img
           src="./assets/images/fb-logo-mb.png"
           alt=""
