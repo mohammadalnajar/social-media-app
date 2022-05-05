de2c3ed2dac7d53120e41192a6ee0f3a6ff584a0
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2022-05-06 00:09:17 +0200

    change icons images

--- client/src/components/leftsidebar/LeftSidebar.jsx
+++ client/src/components/leftsidebar/LeftSidebar.jsx
@@ -13,7 +13,7 @@ const LeftSidebar = () => {
           <SidebarButton
             selected={selected}
             setSelected={setSelected}
-            srcImage="https://robohash.org/mmm"
+            srcImage="./assets/images/category.png"
             option="feed"
           />
         </div>
@@ -21,73 +21,27 @@ const LeftSidebar = () => {
           <SidebarButton
             selected={selected}
             setSelected={setSelected}
-            srcImage="https://robohash.org/mmm"
+            srcImage="./assets/images/user.png"
             option="profile"
           />
         </div>
         <div>
-          <button
-            type="button"
-            className="flex items-center space-x-2 p-2 hover:bg-gray-200 rounded-lg transition-all dark:text-dark-txt dark:hover:bg-dark-third"
-          >
-            <img
-              src="./assets/images/friends.png"
-              alt="Profile "
-              className="w-10 h-10 rounded-full"
-            />
-            <span className="font-semibold">Friends</span>
-          </button>
+          <SidebarButton
+            selected={selected}
+            setSelected={setSelected}
+            srcImage="./assets/images/friends.png"
+            option="Friends"
+          />
         </div>
         <div>
-          <button
-            type="button"
-            className="flex items-center space-x-2 p-2 hover:bg-gray-200 rounded-lg transition-all dark:text-dark-txt dark:hover:bg-dark-third"
-          >
-            <img
-              src="./assets/images/page.png"
-              alt="Profile "
-              className="w-10 h-10 rounded-full"
-            />
-            <span className="font-semibold">Pages</span>
-          </button>
+          <SidebarButton
+            selected={selected}
+            setSelected={setSelected}
+            srcImage="./assets/images/setting.png"
+            option="settings"
+          />
         </div>
-        <div>
-          <button
-            type="button"
-            className="flex items-center space-x-2 p-2 hover:bg-gray-200 rounded-lg transition-all dark:text-dark-txt dark:hover:bg-dark-third"
-          >
-            <img
-              src="./assets/images/memory.png"
-              alt="Profile "
-              className="w-10 h-10 rounded-full"
-            />
-            <span className="font-semibold">Memories</span>
-          </button>
-        </div>
-        <div>
-          <button
-            type="button"
-            className="flex items-center space-x-2 p-2 hover:bg-gray-200 rounded-lg transition-all dark:text-dark-txt dark:hover:bg-dark-third"
-          >
-            <img
-              src="./assets/images/group.png"
-              alt="Profile "
-              className="w-10 h-10 rounded-full"
-            />
-            <span className="font-semibold">Groups</span>
-          </button>
-        </div>
-        <div>
-          <button
-            type="button"
-            className="flex items-center space-x-2 p-2 hover:bg-gray-200 rounded-lg transition-all dark:text-dark-txt dark:hover:bg-dark-third"
-          >
-            <span className="w-10 h-10 rounded-full grid place-items-center bg-gray-300 dark:bg-dark-second">
-              <i className="bx bx-chevron-down" />
-            </span>
-            <span className="font-semibold">See more</span>
-          </button>
-        </div>
+
         <div className="border-b border-gray-200 dark:border-dark-third mt-6" />
       </div>
     </div>
