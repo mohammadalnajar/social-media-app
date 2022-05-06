176bfa21fd18db2615f8223b142d4c3115a0d192
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2022-05-06 22:10:27 +0200

    change bg color for side bar

--- client/src/components/leftsidebar/LeftSidebar.jsx
+++ client/src/components/leftsidebar/LeftSidebar.jsx
@@ -15,7 +15,7 @@ const LeftSidebar = () => {
   }, [currPage]);
 
   return (
-    <div className="w-80 h-full hidden xl:flex flex-col justify-between fixed top-0 left-0 border border-red-600">
+    <div className="w-80 bg-white h-full hidden xl:flex flex-col justify-between fixed top-0 left-0 border ">
       <div className="flex flex-col py-10 px-10">
         <Logo />
         <div className="py-6 my-4">
