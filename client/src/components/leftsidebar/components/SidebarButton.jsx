e577c703070fa95749d3841ad50e2b10fba07e00
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2022-05-06 20:43:53 +0200

    navigate to a user with id

--- client/src/components/leftsidebar/components/SidebarButton.jsx
+++ client/src/components/leftsidebar/components/SidebarButton.jsx
@@ -1,21 +1,31 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
+import { useQuery } from 'react-query';
 
 const SidebarButton = ({ option, selected, setSelected, children }) => {
   const navigate = useNavigate();
   const selectedCheck = selected === option;
 
+  const {
+    data: { data: userData },
+  } = useQuery('fetchUser');
+
   const handleClick = () => {
     setSelected(option);
-    navigate(`/${option}`);
+    if (option === 'profile') {
+      return navigate(`/profile/${userData._id}`);
+    }
+
+    return navigate(`/${option}`);
   };
 
   return (
     <button
       type="button"
       onClick={handleClick}
-      className={`flex items-center space-x-2 w-full my-3 ${
+      className={`flex items-center space-x-2 w-full my-6 ${
         selectedCheck
           ? 'bg-green-secondary dark:bg-dark-third'
           : 'hover:bg-gray-200 dark:hover:bg-dark-third'
